8d16c15050e4ef1250dfa5ecb288be7fc3e8fa22
chrisPixelCraft <rafaelhsieh0813@gmail.com>
2025-03-05 22:09:50 +0000

    fix issue with package

--- get_dataset.sh
+++ get_dataset.sh
@@ -1,2 +1,4 @@
-gdown 1G9KA81LChF3RJQicdGE0mN93yU8yRQX_ -O bookmarks_v2.zip
-unzip bookmarks_v2.zip
+# gdown 1G9KA81LChF3RJQicdGE0mN93yU8yRQX_ -O bookmarks_v2.zip
+# unzip bookmarks_v2.zip
+gdown 1Cqn3uu3oFB8vlOcsTI1uNLUoxtSyZ6In -O bookmark_extra.zip
+unzip bookmark_extra.zip
